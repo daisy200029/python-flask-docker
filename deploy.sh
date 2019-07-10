b861c41004eef0acfd643358e72714c0cbfbba85
daisy <pliu@mozilla.com>
2019-07-10 19:10:41 +0800

    order

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -xv
 VM_NAME="python-flask-docker"
-FIREWALL_NAME="http-server"
-checkVmExist=$(gcloud compute instances list --filter=name:"${VM_NAME}") 
+FIREWALL_NAME="http-server" 
 echo $GCLOUD_SERVICE_KEY | gcloud auth activate-service-account --key-file=-
 gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
 gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
+checkVmExist=$(gcloud compute instances list --filter=name:"${VM_NAME}")
 #gcloud compute firewall-rules create circleci-allow-http --target-tags http-server --allow tcp:8080
 if [[ ${checkVmExist} == *"${VM_NAME}"* ]]
 then
