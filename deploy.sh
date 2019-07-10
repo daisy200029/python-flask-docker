986fcb6ef87c699f853d2f3fbf9c492c44a66820
daisy <pliu@mozilla.com>
2019-07-10 19:08:20 +0800

    set debug

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-
+set -xv
 VM_NAME="python-flask-docker"
 FIREWALL_NAME="http-server"
-checkVmExist=$(gcloud compute instances list "${VM_NAME}") 
+checkVmExist=$(gcloud compute instances list --filter=name:"${VM_NAME}") 
 echo $GCLOUD_SERVICE_KEY | gcloud auth activate-service-account --key-file=-
 gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
 gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
