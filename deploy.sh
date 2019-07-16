3784b5a74513e3ea8f0ab0347217d762f0ca3d38
daisy <pliu@mozilla.com>
2019-07-16 14:43:48 +0800

    add staging env var

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -xv
-VM_NAME="python-flask-docker"
-FIREWALL_NAME="http-server" 
+source staging.sh
+#VM_NAME="python-flask-docker"
+#FIREWALL_NAME="http-server" 
 echo $GCLOUD_SERVICE_KEY | gcloud auth activate-service-account --key-file=-
 gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
 gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
---
+++ staging.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+TAG='0.1.${CIRCLE_BUILD_NUM}'
+IMAGE_NAME='python-flask-docker'
+GITHUB_REPO='daisy200029'
+VM_NAME="python-flask-docker"
+FIREWALL_NAME="http-server" 
